9cc59d0a67b7fb8752cc5cb65aaf27f83baaf8cc
Hsinpa <hsinpa@gmail.com>
2024-07-23 17:03:42 +0800

    finish the sql commit

--- chatbot_python/src/database/chatroom_db.py
+++ chatbot_python/src/database/chatroom_db.py
@@ -8,9 +8,10 @@ class ChatRoomDB:
     Chatroom_Table = 'chatroom'
     Chatbot_Table = 'chatbot_messages'
 
-    async def get_chatroom_info(chatroom_id: int):
+    async def get_chatroom_info(scenario_id: int):
 
         with psycopg.connect(get_conn_uri(), row_factory=dict_row) as conn:
             with conn.cursor() as cur:
-                pass
-                # cur.execute(f"""SELECT id, chatbot_id, narrator_id, """,
+                scenario_fetch = cur.execute("""SELECT id, chatbot_id, narrator_id,
+                scenario_name, background FROM room_scenario WHERE id=%s""", (scenario_id,))
+
